536af480b1a3b72e47b253d609b4881d8803aae2
Nomost <guillaume.soufflet1@viacesi.fr>
2019-04-05 10:06:55 +0200

    write message in files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-projet-crypto
+# How ?
+
+The key is a 6-length lower-case alpha string => so the range is 'aaaaaa' - 'zzzzzz'.
+There is 26^6 = 309 000 000 possibilities.
+By using frequency analysis there is only 26 * 6 = 152 possibilities because we test the key byte separately.
+We know if a combination is good by checking the frequency analysis of characters in the plan text based on French language.
+
--- src/new.js
+++ src/new.js
@@ -6,6 +6,7 @@ const cliProgress = require('cli-progress');
 
 const readFile = util.promisify(fs.readFile);
 const readdir = util.promisify(fs.readdir);
+const writeFile = util.promisify(fs.writeFile);
 
 const bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
 
@@ -17,20 +18,11 @@ const bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
     const words = dictionary.filter(word => word.length > 4);
 
     const encryptedFiles = await readdir('./resources/encrypted_files');
-    const cipher = await readFile(`./resources/encrypted_files/${encryptedFiles[5]}`);
-    
-    // the key is a 6-length lower-case alpha string => so the range is 'aaaaaa' - 'zzzzzz'
-    // There is 26^6 = 309 000 000 possibilities
 
-    // By using frequency analysis there is only 26 * 6 = 152 possibilities because we test the key byte separately
-    // We know if a combination is good by checking the frequency analysis of characters in the plan text based on French language
-    
-    bar.start(100, 0);
-    
     const keyLength = 6;
 
     // https://fr.wikipedia.org/wiki/Fr%C3%A9quence_d%27apparition_des_lettres_en_fran%C3%A7ais
-    const charsFrequency = {
+    const frenchCharsFrequency = {
         'a': 7.11,
         'b': 1.14,
         'c': 3.18,
@@ -59,70 +51,109 @@ const bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
         'z': 0.15
     };
 
-    let charByKeyChar = {
-        0: [],
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: []
+    // https://www.persee.fr/doc/bsnaf_0081-1181_1969_num_1967_1_11724
+    const latinCharsFrequency = {
+        'a': 7.30,
+        'b': 1.32,
+        'c': 4.06,
+        'd': 3.24,
+        'e': 12.14,
+        'f': 1,
+        'g': 0.8,
+        'h': 0.82,
+        'i': 12.64,
+        'l': 2.90,
+        'm': 6.52,
+        'n': 6.74,
+        'o': 6.40,
+        'p': 2.26,
+        'q': 2.26,
+        'r': 5.18,
+        's': 7.88,
+        't': 7.42,
+        'v': 8.92,
+        'x': 0.18
     };
+
+    const languages = [frenchCharsFrequency, latinCharsFrequency];
+
+    bar.start(100, 0);
+
+    const iterations = encryptedFiles.length * keyLength * 26;
+
+    encryptedFiles.map(async (filename, f) => {
+        const cipher = await readFile(`./resources/encrypted_files/${filename}`);
+
+        let charByKeyChar = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: []
+        };
+        
+        // group cipher bytes by key bytes
+        for (let i = 0; i < cipher.byteLength / 2; i++)
+            charByKeyChar[i % keyLength].push(cipher[i]);
     
-    // group cipher bytes by key bytes
-    for (let i = 0; i < cipher.byteLength / 2; i++)
-        charByKeyChar[i % keyLength].push(cipher[i]);
-
-    // array of dec values    
-    let key = [];
-
-    // for each key char 
-    for (let k = 0; k < keyLength; k++) {
-        let frequencyAnalysis = {};
-
-        // for each possible characters (a-z)
-        for (let c = 'a'.charCodeAt(0); c < 'z'.charCodeAt(0) + 1; c++) {
-            const kcMessage = charByKeyChar[k].reduce((acc, encryptedByte) => 
-                acc += String.fromCharCode(c ^ encryptedByte), '');
-
-            const kcWeight = Object.keys(charsFrequency).reduce((acc, char) => {
-                const regex = new RegExp(char, 'g');
-                const occurences = (kcMessage.match(regex) || []).length;
-                return acc += occurences * charsFrequency[char];
-            }, 0);
-
-            frequencyAnalysis[c] = kcWeight;
-
-            const progress = ((k * c) / (26 * 6)) * 100
-            bar.update(progress);
+        // array of dec values    
+        let key = [];
+    
+        // for each key char 
+        for (let k = 0; k < keyLength; k++) {
+            let frequencyAnalysis = {};
+    
+            // for each possible characters (a-z)
+            for (let c = 'a'.charCodeAt(0); c < 'z'.charCodeAt(0) + 1; c++) {
+                const kcMessage = charByKeyChar[k].reduce((acc, encryptedByte) => 
+                    acc += String.fromCharCode(c ^ encryptedByte), '');
+    
+                const kcWeight = Object.keys(frenchCharsFrequency).reduce((acc, char) => {
+                    const regex = new RegExp(char, 'g');
+                    const occurences = (kcMessage.match(regex) || []).length;
+                    return acc += occurences * frenchCharsFrequency[char];
+                }, 0);
+    
+                frequencyAnalysis[c] = kcWeight;
+    
+                const progress = ((f * k * c) / iterations) * 100;
+                bar.update(progress);
+            }
+    
+            const kWithBestWeight = Object.keys(frequencyAnalysis).reduce((a, b) => 
+                frequencyAnalysis[a] > frequencyAnalysis[b] ? a : b);
+            key.push(kWithBestWeight);
         }
 
-        const kWithBestWeight = Object.keys(frequencyAnalysis).reduce((a, b) => 
-            frequencyAnalysis[a] > frequencyAnalysis[b] ? a : b);
-        key.push(kWithBestWeight);
-    }
-
-    let message = '';
-
-    for (let i = 0; i < cipher.byteLength; i++) {
-        const cBloc = cipher[i];
-        const kBloc = key[i % key.length];
-        const xor = cBloc ^ kBloc;
-        message += String.fromCharCode(xor);
-    }    
-
-    const matched = atLeast(words, word => { 
-        if (message.includes(word)) {
-            console.log('word: ', word);
-            return true;
+        let message = '';
+    
+        for (let i = 0; i < cipher.byteLength; i++) {
+            const cBloc = cipher[i];
+            const kBloc = key[i % key.length];
+            const xor = cBloc ^ kBloc;
+            message += String.fromCharCode(xor);
         }
-        else return false;
-    }, 3); 
 
-    if (matched) {
-        console.log('key: ', key);
-        console.log('message: ', message);
-        process.exit();    
-    }
+        const output = 'key: ' + key.map(dec => dec.charCodeAt(0)).join('') + '\n\n' + message;
+        
+        await writeFile(`./decrypted_files/${filename}`, output);
+    
+        // const matched = atLeast(words, word => { 
+        //     if (message.includes(word)) {
+        //         console.log('word: ', word);
+        //         return true;
+        //     }
+        //     else return false;
+        // }, 3); 
+    
+        // if (matched) {
+        //     console.log('key: ', key);
+        //     console.log('message: ', message);
+        // }
+    });
+
+    process.exit();
 })();
 
 const atLeast = (array, predicate, n) => {
